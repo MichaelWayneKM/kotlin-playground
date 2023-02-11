4f176ed9ebb850ad9319d3c95cdb70c26fd70658
Michael Wayne <Kromwell005@gmail.com>
2023-02-11 10:26:53 +0300

    Lazy List Implementation

--- app/src/main/java/com/wkds/basicscodelabcompose/MainActivity.kt
+++ app/src/main/java/com/wkds/basicscodelabcompose/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -72,7 +74,7 @@ fun MyAppPreview() {
 @Composable
 fun HomeScreen() {
 
-    val people = listOf<String>("Michael", "John", "Purity")
+    val people: List<String> = List(1000) { "Person $it" }
     // A surface container using the 'background' color from the theme
     Surface(
         color = Color.White, //MaterialTheme.colorScheme.background
@@ -81,9 +83,9 @@ fun HomeScreen() {
             .padding(vertical = 10.dp)
     ) {
 
-        Column() {
-            for (person in people) {
-                CustomCard(person = person)
+        LazyColumn() {
+            items(items = people) { name ->
+                CustomCard(person = name)
             }
         }
     }
